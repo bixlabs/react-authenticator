d2f7dbe3a5d8e9ed7884d297344c183d2ac606f4
Pedro Medina <pedromedina190@gmail.com>
2020-10-15 20:58:17 -0300

    overall result of today

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AuthProvider } from './hoc/AuthContext/AuthContext';
+import RouterComponent from './routes/Router';
+import WithTheme from './hoc/WithTheme';
+
+function App() {
+
+  return (
+    <WithTheme>
+      <AuthProvider>
+        <RouterComponent />
+      </AuthProvider>
+    </WithTheme>
+  );
+}
+
+export default App;
