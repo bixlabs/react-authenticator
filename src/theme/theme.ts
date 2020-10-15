8d907162db64edcbd5b82533e87094b12cd2db39
Pedro Medina <pedromedina190@gmail.com>
2020-10-15 20:57:20 -0300

    available themes

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+const lightType: "light" | "dark" | undefined = "light";
+const darkType: "light" | "dark" | undefined = "dark";
+
+export const light = {
+  palette: {
+    type: lightType,
+  },
+};
+
+export const dark = {
+  palette: {
+    type: darkType,
+  },
+};
