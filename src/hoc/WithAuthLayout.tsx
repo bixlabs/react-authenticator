60a0da97e58a795ae12bf730a47b3ae4fa89854f
Pedro Medina <pedromedina190@gmail.com>
2020-10-18 19:50:29 -0300

    creates HOC component for authentication views (login, register, recover passwd, etc.)

--- src/hoc/WithAuthLayout.tsx
+++ src/hoc/WithAuthLayout.tsx
@@ -1,30 +1,46 @@
-import { makeStyles } from '@material-ui/core';
-import React, { FC } from 'react';
+import { makeStyles, Paper } from "@material-ui/core";
+import React from "react";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    background: theme.palette.background.default,
+    backgroundColor: theme.palette.background.default,
     color: theme.palette.text.primary,
     display: "flex",
     flex: 1,
     flexDirection: "column",
     minHeight: "100vh",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+  },
+  paper: {
+    background: theme.palette.background.paper,
+    minWidth: "30%",
   },
   formComponent: {
     display: "flex",
     flexDirection: "column",
-    '& *': {
-      margin: '1em'
-    }
-  }
+    "& *": {
+      margin: ".5em",
+    },
+    "& > .top-separated": {
+      margin: "1.5em 0",
+      textAlign: "center",
+      textDecoration: "none",
+      color: theme.palette.text.secondary,
+    },
+  },
 }));
 
-export default function WithAuthenticationLayout(WrappedComponent: FC<{ className: string }>) {
+export default function WithAuthenticationLayout(
+  WrappedComponent: React.FC<{ className: string }>
+) {
   const classes = useStyles();
 
-  return <div className={classes.root}>
-    <WrappedComponent className={classes.formComponent} />
-  </div>
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <WrappedComponent className={classes.formComponent} />
+      </Paper>
+    </div>
+  );
 }
