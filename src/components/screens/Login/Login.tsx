df79cb82ea7596ae23e81c7b0f83b96c758ce875
Pedro Medina <pedromedina190@gmail.com>
2020-10-19 18:01:39 -0300

    extracts email field with debouncing validation uses email field in both signup and login forms

--- src/components/screens/Login/Login.tsx
+++ src/components/screens/Login/Login.tsx
@@ -1,8 +1,12 @@
-import { Button, makeStyles, TextField } from "@material-ui/core";
 import React, { useState } from "react";
+import { Button, makeStyles, TextField } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import actions from "context/AuthContext/actions";
 import { AuthContext } from "context/AuthContext/AuthContext";
+import EmailTextField from "../components/EmailTextField/EmailTextField";
+import { login } from "services/authentication";
+import validateEmail, { EmailStatus } from "utils/validation/validateEmail";
+import { User } from "authenticator/structures/User";
 
 const useStyles = makeStyles((theme) => ({
   buttonLink: {
@@ -21,26 +25,26 @@ const Login: React.FC<{ className: string }> = ({ className }) => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState("");
 
-  const handleAuth = () =>
+  const handleAuth = (user: User) =>
     dispatch({
       type: actions.AUTH_STATE_CHANGED,
-      payload: { email, password },
+      payload: user,
     });
 
   const handleSubmit = () => {
-    handleAuth();
+    const emailValueStatus: EmailStatus = validateEmail(email);
+    if (formIsValid(emailValueStatus, password)) {
+      return login({ email, password }, handleAuth, () => null);
+    }
+  };
+
+  const formIsValid = (emailStatus: EmailStatus, password: string) => {
+    return emailStatus.validForSubmission && password.length > 0;
   };
 
   return (
     <form className={className} noValidate autoComplete="off">
-      <TextField
-        error={false}
-        id="loginUserEmail"
-        label="Email"
-        type="email"
-        value={email}
-        onChange={(e) => setemail(e.target.value)}
-      />
+      <EmailTextField value={email} onChange={setemail} />
       <TextField
         id="loginUserPassword"
         label="Password"
