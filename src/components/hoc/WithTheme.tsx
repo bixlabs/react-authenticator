f2b891687ea8b6b28dc8e5b8380b5f29e668faa6
Pedro Medina <pedromedina190@gmail.com>
2020-10-20 12:54:43 -0300

    adds persisting of last selected theme to localStorage

--- src/components/hoc/WithTheme.tsx
+++ src/components/hoc/WithTheme.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { light, dark } from "theme/theme";
 import { IconButton } from "@material-ui/core";
@@ -8,9 +8,26 @@ interface WithThemeProps {
   children: React.ReactNode;
 }
 
+const getActualThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("lightMode");
+  if (!theme) {
+    return true;
+  }
+
+  return theme === "true";
+};
+
+const updateActualThemeInLocalStorage = (status: boolean) => {
+  localStorage.setItem("lightMode", status.toString());
+};
+
 const WithTheme: React.FC<WithThemeProps> = ({ children }) => {
-  const [themeMode, setThemeMode] = useState(true);
+  const [themeMode, setThemeMode] = useState(getActualThemeFromLocalStorage());
   const theme = createMuiTheme(themeMode ? light : dark);
+
+  useEffect(() => {
+    updateActualThemeInLocalStorage(themeMode);
+  }, [themeMode]);
 
   return (
     <ThemeProvider theme={theme}>
